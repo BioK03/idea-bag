4d0f53664e71ff74764f6e21f252bd10b045b3ea
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2017-02-24 23:19:51 +0100

    Morse Code Generator

---
+++ strings/2 morse code maker/javascript/script.js
@@ -0,0 +1,68 @@
+var morse = {
+  "0": "-----",
+  "1": ".----",
+  "2": "..---",
+  "3": "...--",
+  "4": "....-",
+  "5": ".....",
+  "6": "-....",
+  "7": "--...",
+  "8": "---..",
+  "9": "----.",
+  "a": ".-",
+  "b": "-...",
+  "c": "-.-.",
+  "d": "-..",
+  "e": ".",
+  "f": "..-.",
+  "g": "--.",
+  "h": "....",
+  "i": "..",
+  "j": ".---",
+  "k": "-.-",
+  "l": ".-..",
+  "m": "--",
+  "n": "-.",
+  "o": "---",
+  "p": ".--.",
+  "q": "--.-",
+  "r": ".-.",
+  "s": "...",
+  "t": "-",
+  "u": "..-",
+  "v": "...-",
+  "w": ".--",
+  "x": "-..-",
+  "y": "-.--",
+  "z": "--..",
+  ".": ".-.-.-",
+  ",": "--..--",
+  "?": "..--..",
+  "!": "-.-.--",
+  "-": "-....-",
+  "/": "-..-.",
+  "@": ".--.-.",
+  "(": "-.--.",
+  ")": "-.--.-"
+}
+
+
+function submitForm(){
+    var text = document.querySelector("#text").value;
+
+    var result = convertToMorse(text);
+    document.querySelector("#result").innerHTML = "Result : "+result;
+}
+
+function convertToMorse(text){
+    text = text.toString().toLowerCase();
+    var result = "";
+    for(var i=0; i<text.length; i++){
+        if(text.charAt(i) == " "){
+            result += " ";
+            continue;
+        }
+        result += morse[text.charAt(i)];
+    }
+    return result;
+}
